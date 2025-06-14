d104df9172eb2932f9dbc29f08b1000e92d98ff7
Marcela Godoy <magodoycruz@gmail.com>
2025-06-14 15:55:47 -0300

    utilizando range no loop do desafio

--- desafio_acerte_o_numero/app.py
+++ desafio_acerte_o_numero/app.py
@@ -10,8 +10,7 @@ import random
 
 numero_secreto = random.randint(0,10)
 
-count = 1 # variável auxiliar de contagem
-while count<=3:
+for _ in range(3):
     numero_escolhido = int(input('Escolha um número de 0 a 10: '))
     if numero_escolhido == numero_secreto:
         print(f'Parabéns, você acertou o número!')
@@ -20,5 +19,4 @@ while count<=3:
         print('O número é menor que o escolhido')
     else:
         print('O número é maior que o número escolhido')
-    count +=1
 print(f'Jogo encerrado. O número secreto era {numero_secreto}')
